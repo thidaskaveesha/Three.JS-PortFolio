afa55575a7c913711a592f514855ff4e8908c66c
Thidas Kaveesha <thidaskaveesha@gmail.com>
2024-07-01 15:30:01 +0530

    yoo added a cube

--- FrontEnd/src/pages/Portfolio.jsx
+++ FrontEnd/src/pages/Portfolio.jsx
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useRef } from "react";
 import style from "./Portfolio.module.css";
+import { Canvas, useFrame } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import * as THREE from 'three';
+
+const Box = () => {
+    const mesh = useRef();
+    useFrame(() => {
+        if (mesh.current) {
+            mesh.current.rotation.x += 0.01;
+            mesh.current.rotation.y += 0.01;
+        }
+    });
+    return (
+        <mesh ref={mesh}>
+            <boxGeometry args={[1, 1, 1]} />
+            <meshStandardMaterial color="orange" />
+        </mesh>
+    );
+};
+
 function Portfolio() {
     return (
-        <div>
-            <h2>Portfolio</h2>
-            <p>My Portfolio will go here!</p>
+        <div className={style.Container}>
+            <Canvas>
+                <ambientLight intensity={0.5} />
+                <pointLight position={[10, 10, 10]} />
+                <Box />
+                <OrbitControls />
+            </Canvas>
         </div>
     );
 }
 export default Portfolio;
+
